9b06f76699366025b1148c82b4d45cf34a49cd7c
herbaczewskir <herbaczewski.r@gmail.com>
2023-01-20 13:42:48 +0100

    Praca domowa dzień 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var name1 = "Rafał";
+bool Ewa = true;
+char płeć1 = 'K';
+char płeć2 = 'M';
+var age = 33;
+var age1 = 40;
+
+if (age1 > age)
+{
+    Console.WriteLine();
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Początkujący programista...");
+}
+
+if (Ewa)
+{
+    Console.WriteLine();
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Ewa lat 33");
+}
+if (płeć1 != płeć2)
+{
+    Console.WriteLine();
+    Console.WriteLine("Ewa lat 33");
+}
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine();
+Console.WriteLine("Naciśnij dowlny klawisz aby przejśc dalej...");
+Console.WriteLine();
+Console.ReadKey();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Mam nadzieje że ZADANIE DOMOWE jest zrobione prawidłowo");
+Console.WriteLine();
+Console.ResetColor();
